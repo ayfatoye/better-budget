de31a5b5cb305297f3118c1e4414ceb1a6b11416
ayfatoye <afatoye@tamu.edu>
2025-04-05 21:20:50 -0500

    rounded the edges of log in component

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,7 +5,7 @@ function LandingPage() {
 
     return (
         <div style={{backgroundColor: "#C8A3D8"}} className="w-screen h-screen flex justify-center items-center">
-            <div style={{backgroundColor: "#EAD9D9"}} className="w-[90vw] h-[40vh] md:w-[40vw] md:h-[40vh] xl:w-[30vw] xl:h-[40vh] ">
+            <div style={{backgroundColor: "#EAD9D9"}} className="rounded-2xl w-[90vw] h-[40vh] md:w-[40vw] md:h-[40vh] xl:w-[30vw] xl:h-[40vh] ">
                 <img src="/logo.svg" alt="Logo" className="w-[30%] h-[30%] mx-auto mt-2 mb-2"/>
                 <div className="mt-20">
                     <div className="flex flex-col justify-center items-center">
