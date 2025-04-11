806c3d77d1bae2beabb64e1f0cce36e3faebc5c3
ayfatoye <afatoye@tamu.edu>
2025-04-10 22:24:45 -0500

    added the log in/sign up and forgot password buttons, to get ready for integration with database.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,20 +7,25 @@ function LandingPage() {
         <div style={{backgroundColor: "#C8A3D8"}} className="w-screen h-screen flex justify-center items-center">
             <div style={{backgroundColor: "#EAD9D9"}} className="rounded-2xl w-[90vw] h-[40vh] md:w-[40vw] md:h-[40vh] xl:w-[30vw] xl:h-[40vh] ">
                 <img src="/logo.svg" alt="Logo" className="w-[30%] h-[30%] mx-auto mt-2 mb-2"/>
-                <div className="mt-20">
+                <div className="mt-3">
                     <div className="flex flex-col justify-center items-center">
                         <div className="w-full">
-                            <label className="font-poppins items-start ml-[9%]">Email</label>
+                            <label style={{color: "#33186B"}} className="font-poppins items-start ml-[9%] text-">Email</label>
                         </div>
-                            <input className="bg-white w-[80%]" type="text" id="email"  placeholder="John" required />
+                            <input className="font-poppins bg-white w-[80%] text-black" type="text" id="email"  placeholder=" example@gmail.com" required />
                     </div>
 
                     <div className="flex flex-col justify-center items-center">
                         <div className="w-full">
-                            <label className="font-poppins items-start ml-[9%]">Password</label>
+                            <label style={{color: "#33186B"}} className="font-poppins items-start ml-[9%]">Password</label>
                         </div>
-                            <input className="bg-white w-[80%]" type="text" id="password"  placeholder="John" required />
+                            <input className="font-poppins bg-white w-[80%] text-black" type="text" id="password"  placeholder=" please don't use the word &quot;password&quot;" required />
+
+                            <button type="button" style={{backgroundColor: "#7E4E92"}} className="font-poppins mt-5 w-[75%]">Log In/Sign Up</button>
+                            <p style={{color: "#000000"}} className="font-poppins items-center mt-1">Forgot Password</p>
                     </div>
+
+                    
                 </div>
             </div>
         </div>
