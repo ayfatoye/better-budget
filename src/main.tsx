aa610c29227ac879cb300c021f1af6a39f11889e
ayfatoye <afatoye@tamu.edu>
2025-04-12 23:44:20 -0500

    set foundations for supabase connection

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 // import App from './App.tsx'
 import LandingPage from './pages/LandingPage.tsx'
+import EnvelopesPage from './pages/EnvelopesPage.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
-    <LandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/envelopes" element={<EnvelopesPage />} />
+      </Routes>
+    </BrowserRouter>
+      
   </StrictMode>,
 )
---
+++ src/pages/EnvelopesPage.tsx
@@ -0,0 +1,9 @@
+function EnvelopesPage() {
+  return (
+    <div style={{backgroundColor: "#C8A3D8"}} className="flex justify-center items-center w-screen h-screen">
+      <h1>Envelopes</h1>
+    </div>
+  );
+}
+
+export default EnvelopesPage;
