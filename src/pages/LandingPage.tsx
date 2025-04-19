c85ff2175276b8ce489f2631f32b4da308982d81
ayfatoye <afatoye@tamu.edu>
2025-04-19 14:48:31 -0500

    fixed a vertical overflow issue with the center card on the LandingPage component

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,11 @@
-// import logo from "../assets/logo.svg";
+import {supabase} from "../../supabase-config";
 
 
 function LandingPage() {
 
     return (
         <div style={{backgroundColor: "#C8A3D8"}} className="w-screen h-screen flex justify-center items-center">
-            <div style={{backgroundColor: "#EAD9D9"}} className="rounded-2xl w-[90vw] h-[40vh] md:w-[40vw] md:h-[40vh] xl:w-[30vw] xl:h-[40vh] ">
+            <div style={{backgroundColor: "#EAD9D9"}} className="rounded-2xl w-[90vw] h-[40vh] md:w-[40vw] md:h-[40vh] xl:w-[30vw] xl:h-[40vh] overflow-y-auto">
                 <img src="/logo.svg" alt="Logo" className="w-[30%] h-[30%] mx-auto mt-2 mb-2"/>
                 <div className="mt-3">
                     <div className="flex flex-col justify-center items-center">
@@ -21,7 +21,7 @@ function LandingPage() {
                         </div>
                             <input className="font-poppins bg-white w-[80%] text-black" type="text" id="password"  placeholder=" please don't use the word &quot;password&quot;" required />
 
-                            <button type="button" style={{backgroundColor: "#7E4E92"}} className="font-poppins mt-5 w-[75%]">Log In/Sign Up</button>
+                            <button type="button" style={{backgroundColor: "#7E4E92"}} className="font-poppins mt-[5%] w-[75%] py-2">Log In/Sign Up</button>
                             <p style={{color: "#000000"}} className="font-poppins items-center mt-1">Forgot Password</p>
                     </div>
 
