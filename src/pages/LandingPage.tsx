076f28865276acfed7f11411992385e1b8447d97
Ayo Fatoye <the_scripter@MacBookPro.attlocal.net>
2025-09-21 23:03:00 -0500

    just threw in tailwind

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+    return (
+        <div className="p-8 text-center">
+            <h1>Hi</h1>
+        </div>
+    );
+};
+
+export default LandingPage;
