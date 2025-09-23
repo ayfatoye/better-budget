3828fba5d9849a6b1bc2948e08b9e1def6a06d06
Ayo Fatoye <the_scripter@MacBookPro.attlocal.net>
2025-09-22 19:18:03 -0500

    set up skeleton for the landing page, added the logo and rounded it out

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const LandingPage: React.FC = () => {
     return (
-        <div className="p-8 text-center">
-            <h1>Hi</h1>
+        <div className="flex flex-row w-full">
+            <div className="w-1/2 bg-[#04020E] p-4">Left Side</div>
+            <div className="flex w-1/2 bg-[#8B5CF6] p-4 text-white justify-center items-center">
+            <img src="/logo_1.jpg" alt="Logo" className="h-62 w-62 rounded-[120px]" />
+            </div>
         </div>
     );
 };
